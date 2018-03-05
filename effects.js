c1220176929b50f66ca4a80590c5f79f4a301265
jamajka79 <31252349+jamajka79@users.noreply.github.com>
2018-03-05 18:55:04 +0100

    Initial commit

---
+++ effects.js
@@ -0,0 +1,10 @@
+$("button").on("click", function fade() {
+        $('#1').delay(1000).fadeIn().delay().fadeOut(1000);
+        $('#2').delay(2000).fadeIn().delay().fadeOut(1000);
+        $('#3').delay(3000).fadeIn().delay().fadeOut(1000);
+        $('#4').delay(4000).fadeIn().delay().fadeOut(1000);
+        $('#5').delay(5000).fadeIn().delay().fadeOut(1000);
+        $('#6').delay(6000).fadeIn().delay().fadeOut(1000);
+    
+});
+
